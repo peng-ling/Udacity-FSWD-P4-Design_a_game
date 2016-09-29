88a6128d1f9ae7ec506fa69421f5c7bb9805ca56
Paul Engling <peng-ling@users.noreply.github.com>
2016-09-29 15:36:27 +0200

    class game completed

--- hangman.py
+++ hangman.py
@@ -1,47 +1,67 @@
+# -*- coding: utf-8 -*-
+
+from string import replace
+from wordlist import w
+import random
 
 class game:
 
     def __init__(self):
-        self.secret = "pizza"
-        self.guessedLetters = ""
+        self.secret = random.choice(w)
+        self.guessedLetters=''
+        self.allowedletters='abcdefghijklmnopqrstuvwxyz'
+        self.maxnumberguesses = len(self.secret)
 
     def guess(self, letter):
+
         self.guessedLetters += letter
         return self.guessedLetters
 
+
     def matchresult(self):
+
         match = ""
+
+        for s in range(0,len(self.secret)):
+            match +='*'
+        
         for letter in self.guessedLetters:
-            for secretletter in self.secret:
+            for idx,secretletter in enumerate(self.secret):
                 if letter == secretletter:
-                    match += letter
-                else:
-                    match += '*'
-        return match
+                    match[idx] = letter
+                    
+        return "".join(match)
+
 
     def validateGuess(self,letter):
+
         isguessvalid = ""
+
         if letter == "":
             isguessvalid = "guessed letter was empty"
         elif letter in self.guessedLetters:
-                isguessvalid = "%S has been guessed before" %letter
-        else
-            isguessvalid = "Bingo"
+                isguessvalid = "%s has been guessed before" %letter
+        elif letter not in self.allowedletters:
+                isguessvalid = "%s is not allowed" %letter
+        else:
+            isguessvalid = "guessed letter is valid"
+            
         return isguessvalid
 
 
+    def turnsleft(self):
 
+        tl = self.maxnumberguesses - len(self.guessedLetters)
+        
+        return tl
 
+    def gamestate(self):
 
-schakka = game()
+        if self.turnsleft() > 0:
+            gs = 'running'
+        elif self.turnsleft() == 0 and self.matchresult() == self.secret:
+            gs ='won'
+        else:
+            gs = 'lost'
 
-schakka.guess("a")
-print (schakka.guessedLetters)
-print (schakka.matchresult())
-schakka.guess("b")
-
-print (schakka.validateGuess(''))
-print (schakka.validateGuess('b'))
-
-print (schakka.guessedLetters)
-print (schakka.matchresult())
+        return gs
